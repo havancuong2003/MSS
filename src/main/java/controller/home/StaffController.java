3e54aeb12319c10d9f8ad9b075b48be399c6407e
havancuong2003 <havancuong07122003@gmail.com>
2024-05-29 11:14:05 +0700

    update: forward data for user profile after login

--- src/main/java/controller/home/StaffController.java
+++ src/main/java/controller/home/StaffController.java
@@ -1,16 +1,55 @@
 package controller.home;
 
+import dal.ProfileDBContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Account;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+
 @WebServlet("/staff/dashboard")
 public class StaffController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Account account = (Account) session.getAttribute("account");
+
+
+        ProfileDBContext dao = new ProfileDBContext();
+
+        int getAccountID = account.getId();
+        String accountId = String.valueOf(getAccountID);
+        Account acc = dao.getAccountByID(accountId);
+        if (acc.getAvatar() == null) {
+            req.setAttribute("img", "");
+        } else {
+            try {
+                InputStream inputStream = dao.getPhoto(accountId);
+                if (inputStream != null) {
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                    byte[] photoBytes = outputStream.toByteArray();
+                    String photoBase64 = Base64.getEncoder().encodeToString(photoBytes);
+                    req.setAttribute("img", "not null");
+                    req.setAttribute("photoBase64", photoBase64);
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        req.setAttribute("account", account);
         req.getRequestDispatcher("../views/dashboard/staff.jsp").forward(req, resp);
     }
 
