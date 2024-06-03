1c4a299622e75892f2e72a2a2fc29f590f7dc05f
Nguyen Thi Dieu Linh <linhntdhe172208@fpt.edu.vn>
2024-06-03 17:13:34 +0700

    update: update main class

--- src/main/java/dal/Main.java
+++ src/main/java/dal/Main.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-
+        AccountDBContext a  =new AccountDBContext();
+        a.insertAccount("ed","a","a","2345676242","haa@gmail.com",3,1);
     }
 }
