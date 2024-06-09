764d9b72f68527e799a77693d6c61aaefe956fef
Nguyen Thi Dieu Linh <linhntdhe172208@fpt.edu.vn>
2024-06-10 01:09:47 +0700

    add quiz: for exercise and test

--- src/main/java/model/Answer.java
+++ src/main/java/model/Answer.java
@@ -5,4 +5,46 @@ public class Answer {
     private String answer;
     private int status;
     private Question question;
+
+    public Answer() {
+    }
+
+    public Answer(int answerid, Question question, int status, String answer) {
+        this.answerid = answerid;
+        this.question = question;
+        this.status = status;
+        this.answer = answer;
+    }
+
+    public int getAnswerid() {
+        return answerid;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setAnswerid(int answerid) {
+        this.answerid = answerid;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
 }
---
+++ src/main/java/model/Quiz.java
@@ -0,0 +1,70 @@
+package model;
+
+public class Quiz {
+    private int questionid;
+    private String question;
+    private int typequestion;
+    private int exerciseid;
+    private int courseid;
+    private int status;
+
+    public Quiz() {
+    }
+
+    public Quiz(int questionid, int exerciseid, String question, int typequestion, int courseid, int status) {
+        this.questionid = questionid;
+        this.exerciseid = exerciseid;
+        this.question = question;
+        this.typequestion = typequestion;
+        this.courseid = courseid;
+        this.status = status;
+    }
+
+    public int getQuestionid() {
+        return questionid;
+    }
+
+    public void setQuestionid(int questionid) {
+        this.questionid = questionid;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public int getTypequestion() {
+        return typequestion;
+    }
+
+    public void setTypequestion(int typequestion) {
+        this.typequestion = typequestion;
+    }
+
+    public int getExerciseid() {
+        return exerciseid;
+    }
+
+    public void setExerciseid(int exerciseid) {
+        this.exerciseid = exerciseid;
+    }
+
+    public int getCourseid() {
+        return courseid;
+    }
+
+    public void setCourseid(int courseid) {
+        this.courseid = courseid;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
