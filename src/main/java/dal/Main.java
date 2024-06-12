123f5f5e26516a482739f0dc99ade1d105212468
havancuong2003 <havancuong07122003@gmail.com>
2024-06-12 08:10:08 +0700

    fix: fix model

--- src/main/java/dal/Main.java
+++ src/main/java/dal/Main.java
@@ -3,6 +3,7 @@ package dal;
 import model.Account;
 import model.Course;
 import model.Student;
+import model.Term;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -11,8 +12,17 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-
+        CurriculumDBContext c = new CurriculumDBContext();
+        for (Term term : c.getTermForCurriculum(1)) {
+            System.out.println("Term ID: " + term.getId());
+            System.out.println("Term Detail: " + term.getTermDetail());
+            System.out.println("Number of Courses: " + term.getCourses().size());
+            System.out.println("Courses:");
+            for (Course course : term.getCourses()) {
+                System.out.println(" - Course ID: " + course.getId());
+            }
+            System.out.println("------------------------");
+        }
     }
-
 
 }
